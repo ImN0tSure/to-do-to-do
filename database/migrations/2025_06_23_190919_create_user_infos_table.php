1c75cfb258723c63878d9739b048c065fd95bf7c
ImN0tSure <gghyhg64@gmail.com>
2025-06-23 23:29:49 +0300

    models and migrations

---
+++ database/migrations/2025_06_23_190919_create_user_infos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('surname');
+            $table->string('name');
+            $table->string('patronymic')->nullable();
+            $table->string('avatar_img');
+            $table->string('nickname')->nullable()->unique();
+            $table->string('phone')->nullable()->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_infos');
+    }
+};
---
+++ database/seeders/ProjectParticipantsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectParticipantsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
